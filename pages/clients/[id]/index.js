6f577e676b9bd91e47f81336186f13644767f949
Masami <msm-k0304@hotmail.com>
2023-06-22 12:12:26 +1200

    rounting session done

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,12 +3,21 @@ import { useRouter } from 'next/router'
 function ClientProjectsPage() {
 
   const router = useRouter()
-  console.log(router.pathname)
-  console.log(router.query)
   
-  return <div>
+  console.log(router.query)
+
+  function loadProjectHandlre() {
+    // load data...
+    router.push({
+      pathname: '/clients/masami/projecta',
+      query: { id: 'masami', clientprojectid: 'projecta'}
+    })
+  }
+  
+  return (<div>
     <h1>The Projects for a Given Client</h1>
-  </div>
+    <button onClick={loadProjectHandlre}>Load Project A</button>
+  </div>)
 }
 
 export default ClientProjectsPage
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,6 +1,23 @@
+import Link from 'next/link'
+
 function ClientsPage() {
+  const clients = [
+    { id:'masami', name: 'Stonebridge'},
+    { id:'chloe', name: 'Stone'},
+    { id:'sarah', name: 'Bridge'},
+  ]
   return <div>
     <h1>The Clients Page</h1>
+      <ul>
+        {clients.map(client => ( 
+        <li key={client.id}>
+          <Link href={{
+            pathname: '/clients/[id]',
+            query: {id: client.id},
+          }}>{client.name}</Link>
+        </li>
+        ))}
+      </ul>
   </div>
 }
 
